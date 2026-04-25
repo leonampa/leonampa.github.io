64344880e69d721335d03ccd6c35a9253bdc1fca
leonampa <leonidampa@gmail.com>
2026-04-25 18:25:53 +0300

    typo

--- home/en/script.js
+++ home/en/script.js
@@ -3,7 +3,7 @@ const socialLinks = [
     {
         name: "GitHub",
         url: "https://github.com/leonampa",
-        icon: ",../github.png"
+        icon: "../github.png"
     },
     {
         name: "Email",
